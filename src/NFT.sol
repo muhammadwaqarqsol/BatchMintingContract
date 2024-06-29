0121a823be3d2ba3233934baa8986484692d806b
root <root@admin>
2024-06-29 18:10:59 +0500

    erc1155 contract added

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,63 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract NFT is ERC721URIStorageUpgradeable {
-    uint256 public _tokenIds; // Tracking the no of tokens minted
+contract MyToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable {
+    // Mapping from token ID to its custom URI
+    mapping(uint256 => string) private _tokenURIs;
 
-    /**
-     * @notice Enables to initialize the contract
-     * @param name  collection name
-     * @param symbol collection symbolr
-     *
-     * intialize contract by owner.
-     * Function should be perform by contract owner to initiliaze it.
-     */
-    function initialize(string memory name, string memory symbol) public initializer {
-        ERC721Upgradeable.__ERC721_init(name, symbol); // Do not forget this call!
+    // Initialize function instead of constructor
+    function initialize(address initialOwner) public initializer {
+        __ERC1155_init("");
+        __Ownable_init(msg.sender);
+        transferOwnership(initialOwner);
     }
 
-
-    /**
-     * @notice Enables users to create NFT Token
-     * @param tokenURI  NFT detail of ipfs url
-     *
-     * user create the NFT.
-     * Function should be perform by user to create token.
-     */
-    function createToken(string memory tokenURI) public returns (uint256) {
-        require(msg.sender != address(0),"Has Zero Address");
-        uint256 newTokenId = _tokenIds+=1; // The new token id is the current value of the counter
-        _mint(msg.sender, newTokenId); // mint the token to the sender
-        _setTokenURI(newTokenId, tokenURI); // set the tokenURI to the tokenId.
-        return newTokenId;
+    // Function to set the URI for a specific token ID
+    function setURI(uint256 tokenId, string memory newuri) public onlyOwner {
+        _tokenURIs[tokenId] = newuri;
     }
 
+    // Override the uri function to return the custom URI for each token ID
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
 
-    /**
-     * @notice Enables users to create and transfer nft
-     * @param _creator  creator address
-     * @param _to  To transfer address 
-     * @param _tokenURI NFT url of ipfs data
-     *
-     * buyer mint and transfer to himself using lazymint functionality.
-     * Function should be perform by buyer to buy the nft and transfer to himself.
-     */
-    function mintAndTransfer(
-        address _creator,
-        address _to,
-        string memory _tokenURI
-    )  external returns(uint)  {
-        require(_creator != address(0),"Has Zero Address");
-        require(_to != _creator,"Have same Address");
-        require(_to != address(0),"Have zero address");
-        uint256 newTokenId = _tokenIds+=1;
-        _safeMint(_creator, newTokenId);
-        _setTokenURI(newTokenId, _tokenURI);
-        _transfer(_creator, _to, newTokenId);
-        return(newTokenId);
+    // Function to mint a new token
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    // Function to mint multiple new tokens
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
     }
 }
