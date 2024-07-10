e98a8cfdf94564d6f5e2b0c1295a6ef3e606db6c
muhammad Waqar <muwaqar.qsol@gmail.com>
2024-07-11 02:29:24 +0500

    updating our code

--- src/USDT.sol
+++ src/USDT.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDTToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("usdt", "USDT") Ownable(msg.sender) {}
+    
+    /**
+     * @notice Enable to mint new USDT tokens
+     * @param to  to address you want to mint tokens
+     * @param amount token amount you want to mint
+     *
+     * Can be mint by anyone to any address
+     */
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount * 10 ** 6);
+    }
+}
--- src/libraries/orderTypes.sol
+++ src/libraries/orderTypes.sol
@@ -15,4 +15,28 @@ library orderTypes {
         bytes32 s;
     }
 
+      struct LazyMakeOrder {
+        bool isOrderAsk; // true --> ask / false --> bid
+        address signer; // signer of the maker order
+        address baseAccount; // address for the payment
+        address nftContract; // collection address
+        address nftOwner;
+        address accountOwner;
+        uint256 price; // price (used as )
+        // uint256 tokenId; // id of the token
+        uint256 tax; // service fee for the owner of contract
+        // uint256 royalty; // royalty amount sent to creator
+        uint256 nonce; // order nonce (must be unique unless new maker order is meant to override existing one e.g., lower ask price)
+        uint8 v; // v: parameter (27 or 28)
+        bytes32 r; // r: parameter
+        bytes32 s; // s: parameter
+    }
+
+    struct LazyTakerOrder {
+        bool isOrderAsk; // true --> ask / false --> bid
+        address taker; // msg.sender
+        uint256 price; // final price for the purchase + tax
+        string  uri;//uri for token
+    }
+
 }
